853517665a9904f84e8a3a7b21f1d57f773e00fd
ispyjun <57784531+ispyjun@users.noreply.github.com>
2020-08-25 15:29:33 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -3,7 +3,23 @@ toDoInput=toDoForm.querySelector("input"),
 toDoList=document.querySelector(".js-toDoList");
 
 const TODOS_LS='toDos';
-const toDos=[];
+let toDos=[];
+let uniqueNumber=0;    
+
+function filterFn(toDo){
+    return toDo.id===1;
+}
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li= btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos=toDos.filter(function(toDo){
+        return toDo.id!== parseInt(li.id);
+    });
+    toDos = cleanToDos
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
@@ -12,9 +28,11 @@ function saveToDos(){
 function paintToDo(text){
     const li=document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText="❌";
     const span=document.createElement("span");
-    const newId = toDos.length+1;
+    newId=uniqueNumber++;
+    
+    delBtn.innerText="❌"
+    delBtn.addEventListener("click",deleteToDo);  
     span.innerText=text;
     li.appendChild(span);
     li.appendChild(delBtn);
